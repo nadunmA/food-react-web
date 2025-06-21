883803431c07e784e3154875222b423f90daa6f5
nadunmA <naduncode404@outlook.com>
2025-06-22 00:21:27 +0530

    add hero image

--- food/src/Components/Hero/Hero.jsx
+++ food/src/Components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import HeroJpeg from "./../../assets/hFood.png";
 
 const Hero = () => {
   return (
@@ -7,12 +8,12 @@ const Hero = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
           {/*text content section*/}
 
-          <div>
+          <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left">
             <h1 className="text-8xl lg:text-[10rem] font-bold bg-clip-text text-transparent bg-gradient-to-b from-black to-gray-900">
               Try <br />
               Now
             </h1>
-            <p className="text-sm text-black font-sans">
+            <p className="text-sm text-black font-sans ml-2.5">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores
               <br />
               odio nemo veritatis eveniet quas ut dolorum provident
@@ -30,6 +31,9 @@ const Hero = () => {
           </div>
 
           {/*image section*/}
+          <div className="min-h[450px] flex justify-center items-center relative order-1 sm:order-2">
+            <img src={HeroJpeg} alt="Hero img" className="max-w-[450px] w-full mx-auto sm:scale-125 shadow-1"/>
+          </div>
         </div>
       </div>
     </div>
