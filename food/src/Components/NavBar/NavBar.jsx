0ea5325caf5e16186a94694eb9b111b3b2399599
nadunmA <naduncode404@outlook.com>
2025-06-22 23:56:25 +0530

    buttons changes

--- food/src/Components/NavBar/NavBar.jsx
+++ food/src/Components/NavBar/NavBar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Logo from "../../assets/llogo.png";
-
 import { FaBars } from "react-icons/fa";
 
 const Menu = [
@@ -31,67 +30,100 @@ const Menu = [
   },
   {
     id: 6,
-    name: ["Dolphin Kottu"],
+    name: ["Naci Gureng"],
     link: "/#",
   },
 ];
 
 const NavBar = () => {
-
   const [open, setOpen] = React.useState(false);
 
   const handleOpen = () => {
     setOpen(!open);
-  }
+  };
 
   return (
     <>
+      <style jsx>{`
+        @keyframes shake3856 {
+          0% {
+            transform: translate(0);
+          }
+          20% {
+            transform: translate(-2px, 2px);
+          }
+          40% {
+            transform: translate(-2px, -2px);
+          }
+          60% {
+            transform: translate(2px, 2px);
+          }
+          80% {
+            transform: translate(2px, -2px);
+          }
+          100% {
+            transform: translate(0);
+          }
+        }
+        .custom-button:hover {
+          animation: shake3856 0.3s linear infinite both;
+        }
+      `}</style>
+
       <div className="bg-white">
         <div className="container py-2">
           <div className="flex justify-between">
             {/* Logo section */}
-
             <div>
-              <a href="# ">
+              <a href="#">
                 <img src={Logo} alt="logo" className="w-23 ml-5" />
               </a>
             </div>
 
-            {/* Link section */}
-            <div className="hidden lg:flex justify-between items-center gap-4 shadow-1">
+            {/* Button section */}
+            <div className="hidden lg:flex justify-between items-center gap-4">
               <ul className="hidden lg:flex justify-between items-center gap-4">
                 {Menu.map((menu) => (
                   <li key={menu.id}>
-                    <a href={menu.link} className="inline-block text-xl p-4">
+                    <button
+                      onClick={() => (window.location.href = menu.link)}
+                      className="custom-button h-12 w-32 border-none rounded-full text-lg text-white font-medium hover:bg-black transition-colors"
+                      style={{ background: "#16a34a" }}
+                    >
                       {menu.name}
-                    </a>
+                    </button>
                   </li>
                 ))}
               </ul>
             </div>
 
-            {/*Responsive part*/}
+            {/* Responsive part */}
             <div className="lg:hidden">
-              <button onClick={handleOpen}>
-                <FaBars className="text-3xl mt-8" />
+              <button
+                onClick={handleOpen}
+                className="custom-button h-12 w-32 border-none rounded-full bg-green-600 text-lg text-white font-medium hover:bg-black transition-colors"
+                style={{ background: "#16a34a" }}
+              >
+                <FaBars className="text-xl mx-auto" />
               </button>
-              {/*DropDown menu*/}
-              {open && <div>
-                
-                <ul className="bg-white space-y-3 p-4 rounded-md shadow-md absolute right-43 top-24 z-50 shadow-1">
-                  {Menu.map((menu) => (
-                    <li key={menu.id}>
 
-                      <a href={menu.link} className="shadow-1 inline-block text-xl p-4 hover:bg-green-500 rounded-md hover:text-white w-full">
-                        {menu.name}
-                      </a>
-
-                    </li>
-                  ))}
-                </ul>
-
-                </div>}
-              
+              {/* DropDown menu */}
+              {open && (
+                <div>
+                  <ul className="bg-white space-y-3 p-4 rounded-md shadow-md absolute right-43 top-24 z-50 shadow-1">
+                    {Menu.map((menu) => (
+                      <li key={menu.id}>
+                        <a
+                          href={menu.link}
+                          className="shadow-1 inline-block text-xl p-4 hover:bg-green-600 rounded-md hover:text-white w-full"
+                        >
+                          {menu.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
             </div>
           </div>
         </div>
